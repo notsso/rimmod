d74958cda4c175ee0f88391163418184efbd3426
amoruch <roma.usha@yandex.ru>
2026-08-02 10:38:49 +0300

    hunter action depends on animal size + message

--- lotr/Hunter.cs
+++ lotr/Hunter.cs
@@ -8,6 +8,8 @@ using Verse.AI;
 
 using RimWorld;
 
+using UnityEngine;
+
 namespace lotr {
     public abstract class Beyonder_Hediff : HediffWithComps {
         private int sanityTickCounter = 0;
@@ -108,22 +110,39 @@ namespace lotr {
         }
     }
 
+    // Harmony patch - отслеживает 'действие' охотника
     [HarmonyPatch(typeof(Pawn_JobTracker), "EndCurrentJob")]
     public static class Patch_Pawn_JobTracker_EndCurrentJob {
+        private static float factor { get; } = 0.1f;
+
         [HarmonyPrefix]
-        public static void Prefix(Pawn_JobTracker __instance, JobCondition condition, bool startNewJob, bool canReturnToPool, ref bool __state) {
+        public static void Prefix(Pawn_JobTracker __instance, JobCondition condition, bool startNewJob, bool canReturnToPool, ref float __state) {
+            __state = 0.0f;
+
             if (__instance.curJob != null && __instance.curJob.def == JobDefOf.Hunt && condition == JobCondition.Succeeded) {
-                __state = true;
+                __state = 1.0f;
+
+                if (__instance.curJob.targetA.Thing is Pawn victim) {
+                    __state = victim.RaceProps.baseBodySize; // в зависимости от размера добычи, усвоение меняется
+                }
             }
         }
 
         [HarmonyPostfix]
-        public static void Postfix(Pawn_JobTracker __instance, JobCondition condition, bool startNewJob, bool canReturnToPool, ref bool __state, Pawn ___pawn) {
-            if (__state && ___pawn != null && ___pawn.IsColonist) {
+        public static void Postfix(Pawn_JobTracker __instance, JobCondition condition, bool startNewJob, bool canReturnToPool, ref float __state, Pawn ___pawn) {
+            if (__state > 0.01f && ___pawn != null && ___pawn.IsColonist) {
                 Hediff hediff = ___pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named("Hunter9_Hediff"));
 
                 if (hediff != null) {
-                    hediff.Severity += 0.5f;
+                    float victimBodySize = __state;
+                    float severityIncrement = factor * victimBodySize;
+                    // severityIncrement = Mathf.Clamp(severityIncrement, 0.02f, 0.40f);
+
+                    hediff.Severity += severityIncrement;
+
+                    string messageText = $"После действия, {___pawn.LabelShortCap} усвоил свое зелье на {severityIncrement.ToStringPercent()}!";
+
+                    Messages.Message(messageText, ___pawn, MessageTypeDefOf.SilentInput, historical: false);
                 }
             }
         }
