4cf002619bfbbf672e0fb10e9e3ad6504c8b630a
amoruch <roma.usha@yandex.ru>
2026-08-27 13:51:53 +0300

    Bard path: hediffs, potions, recipes, ingredients, research (updated alignment of all tree), textures

---
+++ lotr/Paths/Bard.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using HarmonyLib;
+
+using Verse;
+using Verse.AI;
+
+using RimWorld;
+
+using UnityEngine;
+
+namespace lotr {
+    // Последовательность 9 - Бард
+    public class Bard9_Hediff : Beyonder_Hediff {
+
+        public Bard9_Hediff() {
+            // способы действия: -
+            maxProgressPerCategory = 0.8f;
+
+            // анти-действия: -
+        }
+    }
+
+    // Последовательность 8 - Проситель света
+    public class Bard8_Hediff : Beyonder_Hediff {
+
+        public Bard8_Hediff() {
+            // способы действия: -
+            maxProgressPerCategory = 0.8f;
+
+            // анти-действия: -
+        }
+    }
+
+    // Последовательность 7 - Солнечный первосвященник
+    public class Bard7_Hediff : Beyonder_Hediff {
+
+        public Bard7_Hediff() {
+            // способы действия: -
+            maxProgressPerCategory = 0.4f;
+
+            // анти-действия: -
+        }
+    }
+
+    // Последовательность 6 - Нотариус
+    public class Bard6_Hediff : Beyonder_Hediff {
+
+        public Bard6_Hediff() {
+            // способы действия: -
+            maxProgressPerCategory = 0.8f;
+
+            // анти-действия: -
+        }
+    }
+
+    // Последовательность 5 - Священник света
+    public class Bard5_Hediff : Beyonder_Hediff {
+        public Bard5_Hediff() {
+            // способы действия: -
+            maxProgressPerCategory = 0.8f;
+
+            // анти-действия: -
+        }
+    }
+
+    // Последовательность 4 - Незатенённый
+    public class Bard4_Hediff : Beyonder_Hediff {
+        public Bard4_Hediff() {
+            // способы действия: -
+            maxProgressPerCategory = 1f;
+
+            // анти-действия: -
+        }
+
+    }
+}
